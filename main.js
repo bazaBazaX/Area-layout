001d9eb22555fc1a2a9b3cdaef284d8409c06c87
andriy <you@example.com>
2026-05-02 12:13:16 +0300

    initial layout

---
+++ main.js
@@ -0,0 +1,17 @@
+const section = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+section.forEach((section) => observer.observe(section));
